bc2df79c0166bd8323439380ea2777bc6af8711a
Matthew Edell <edell.matthew@gmail.com>
2020-08-19 12:13:17 -0400

    Initial commit, made skeleton app

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: false,
+    port: 3000
+  }
+};
